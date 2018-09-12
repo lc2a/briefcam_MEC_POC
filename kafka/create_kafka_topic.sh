4286da24e8c3ae12944f1bdba2ebec9a0b457e2a
Sriram Sridhar <>
2018-09-12 09:40:57 -0400

    Create kafka topic.

--- kafka/create_kafka_topic.sh
+++ kafka/create_kafka_topic.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/opt/kafka/bin/kafka-topics.sh --create --zookeeper mec-poc:2181 --replication-factor 1 --partitions 10 --topic topic_key
+/opt/kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --partitions 10 --topic $topic_key
