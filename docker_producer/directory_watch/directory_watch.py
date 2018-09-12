f2713d76c670d0d86516339a4165e1fdb280042a
Sriram Sridhar <>
2018-09-12 11:17:59 -0400

    Cleaned up Docker Producer.

--- docker_producer/directory_watch/directory_watch.py
+++ docker_producer/directory_watch/directory_watch.py
@@ -2,50 +2,57 @@ import time
 import os
 import sys
 from sys import path
-
 path.append(os.getcwd())
-
 import time
-
-from kafka_post.kafka_producer import post_filename_to_a_kafka_topic
-
+from kafka_post.kafka_producer import Producer
 from log.log_file import logging_to_console_and_syslog
+#from collections import defaultdict
 
-def process_new_file(file_name):
-    # post the file_name into a kafka topic kafka_topic_name
-    post_filename_to_a_kafka_topic(file_name)
+class Directory_watch:
+    def __init__(self):
+        self.before={}
+        self.after={}
+        self.producer_instance=Producer()
+        self.video_file_path = None
+        while self.video_file_path is None:
+            self.video_file_path = os.getenv("video_file_path_key", default=None)
+        logging_to_console_and_syslog(("video_file_path={}".format(self.video_file_path)))
 
+    def cleanup(self):
+        self.producer_instance.close_producer_instance()
 
-def watch_a_directory(video_file_path):
-    before = dict([(f, None) for f in os.listdir(video_file_path)])
-    while 1:
-        time.sleep(1)
-        after = dict([(f, None) for f in os.listdir(video_file_path)])
-        added = [f for f in after if not f in before]
-        removed = [f for f in before if not f in after]
-        if added:
-            logging_to_console_and_syslog("Added: " + str(added))
-            for filename in added:
-                process_new_file(video_file_path + '/' + filename)
-        if removed:
-            logging_to_console_and_syslog("Removed: " + str(removed))
-        before = after
+    def process_new_file(self,file_name):
+        # post the file_name into a kafka topic kafka_topic_name
+        self.producer_instance.post_filename_to_a_kafka_topic(file_name)
+
+    def watch_a_directory(self):
+        self.before = dict([(f, None) for f in os.listdir(self.video_file_path)])
+        while True:
+            time.sleep(1)
+            self.after = dict([(f, None) for f in os.listdir(self.video_file_path)])
+            added = [f for f in self.after if not f in before]
+            removed = [f for f in self.before if not f in after]
+            if added:
+                logging_to_console_and_syslog("Added: " + str(added))
+                for filename in added:
+                    self.process_new_file(self.video_file_path + '/' + filename)
+            if removed:
+                logging_to_console_and_syslog("Removed: " + str(removed))
+            self.before = self.after
 
 
 if __name__ == "__main__":
+    watch_directory_instance = None
     try:
-        # if proceed_with_execution() == True:
-        video_file_path = os.getenv("video_file_path_key", default=None)
-        logging_to_console_and_syslog(("video_file_path={}".format(video_file_path)))
-        # watch_a_directory(video_file_path)
-        watch_a_directory(video_file_path)
-    except NoProcessExcept as e:
-        logging_to_console_and_syslog(("No Process exception occurred.{}".format(e)))
+        watch_directory_instance = Directory_watch()
+        watch_directory_instance.watch_a_directory()
     except KeyboardInterrupt:
         logging_to_console_and_syslog("You terminated the program by pressing ctrl + c")
     except BaseException:
-        logging_to_console_and_syslog("Base Exception occurred")
+        logging_to_console_and_syslog("Base Exception occurred" + sys.exc_info()[0])
     except:
-        logging_to_console_and_syslog("Unhandled exception")
+        logging_to_console_and_syslog("Unhandled exception" + sys.exc_info()[0])
     finally:
+        if watch_directory_instance:
+            watch_directory_instance.cleanup()
         exit
--- docker_producer/directory_watch/kafka_post/kafka_producer.py
+++ docker_producer/directory_watch/kafka_post/kafka_producer.py
@@ -1,27 +1,44 @@
 from kafka import KafkaProducer
 import os
 import sys
-sys.path.append("..") # Adds higher directory to python modules path.
-
+sys.path.append("..")  # Adds higher directory to python modules path.
 from log.log_file import logging_to_console_and_syslog
 
+class Producer:
+    def __init__(self):
+        self.producer_instance=None
+        self.broker_name = None
+        self.topic = None
+        self.create_producer_instance()
 
-broker_name = os.getenv("broker_name_key", default=None)
-logging_to_console_and_syslog("broker_name={}".format(broker_name))
-topic = os.getenv("topic_key", default=None)
-logging_to_console_and_syslog("topic={}".format(topic))
+    def create_producer_instance(self):
+        while self.broker_name is None and self.topic is None:
+            self.broker_name = os.getenv("broker_name_key", default=None)
+            self.topic = os.getenv("topic_key", default=None)
+        self.producer_instance = KafkaProducer(bootstrap_servers=self.broker_name)
+        logging_to_console_and_syslog("broker_name={}".format(self.broker_name))
+        logging_to_console_and_syslog("topic={}".format(self.topic))
 
-def post_filename_to_a_kafka_topic(filename):
-    producer = KafkaProducer(bootstrap_servers=broker_name)
+    def post_filename_to_a_kafka_topic(self,filename):
+        if filename is None or len(filename) == 0:
+            logging_to_console_and_syslog("filename is None or invalid")
+            return
+        if self.producer_instance is None:
+            logging_to_console_and_syslog("Producer instance is None")
+            return
 
+        # Asynchronously produce a message, the delivery report callback
+        # will be triggered from poll() above, or flush() below, when the message has
+        # been successfully delivered or failed permanently.
+        logging_to_console_and_syslog(
+            "Posting filename={} into kafka broker={}, topic={}".format(filename,
+                                                                        self.broker_name,
+                                                                        self.topic))
+        self.producer_instance.send(self.topic, filename.encode('utf-8'))
 
-    # Asynchronously produce a message, the delivery report callback
-    # will be triggered from poll() above, or flush() below, when the message has
-    # been successfully delivered or failed permanently.
-    logging_to_console_and_syslog(
-        "Posting filename={} into kafka broker={}, topic={}".format(filename, broker_name, topic))
-    producer.send(topic, filename.encode('utf-8'))
+        # Wait for any outstanding messages to be delivered and delivery report
+        # callbacks to be triggered.
+        self.producer_instance.flush()
 
-    # Wait for any outstanding messages to be delivered and delivery report
-    # callbacks to be triggered.
-    producer.flush()
+    def close_producer_instance(self):
+        self.producer.close()
